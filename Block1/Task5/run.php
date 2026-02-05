01e6baa6e5fc3035bccf50f95bbbd7cdc4aeadbd
Nick Felker <felker.nick@bk.ru>
2026-02-05 20:47:11 +0600

    gc settings from console. Task 5

--- Block1/Task5/run.php
+++ Block1/Task5/run.php
@@ -1,17 +1,28 @@
 <?php
-//gc_disable();
+
+
+$options = getopt("", ["mode:"]);
 
 class Node
 {
     public ?Node $ref = null;
+
     public function __construct(?Node $ref = null)
     {
         $this->ref = $ref;
     }
 }
 
-$start_time = microtime(true);
 
+if (!isset($options["mode"])) {
+    die("Dont specified --mode\n");
+}
+
+if ($options["mode"] === "no-gc") {
+    gc_disable();
+}
+
+$start_time = microtime(true);
 for ($i = 0; $i < 50_000; ++$i) {
     $a = new Node();
     $b = new Node();
@@ -19,12 +30,14 @@ for ($i = 0; $i < 50_000; ++$i) {
     $a->ref = $b;
     $b->ref = $a;
 
-    if($i % 1000 == 0 && $i !== 0)
-    {
+    if ($i % 1000 == 0 && $i !== 0) {
         echo memory_get_usage(true) / 1024 . "\n";
     }
 
 }
-
 $end_time = microtime(true);
 echo round(($end_time - $start_time) * 1000, 5) . "\n";
+
+
+
+
