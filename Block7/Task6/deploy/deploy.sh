4c5d6bbae16ccc2d0a75ba80f4a2192581b9a5b1
Nick Felker <felker.nick@bk.ru>
2026-03-24 22:25:22 +0600

    repair deploy bash files. Task6

--- Block7/Task6/deploy/deploy.sh
+++ Block7/Task6/deploy/deploy.sh
@@ -2,13 +2,13 @@
 
 source .env
 
-scp -r . $SERVER_USER@$SERVER_HOST:~/task5
+scp -r . $SERVER_USER@$SERVER_HOST:~/task6
 
 ssh $SERVER_USER@$SERVER_HOST << EOF
     cd $SERVER_PATH
     docker compose down
     docker compose build
     docker compose up -d
-    docker exec -i task5-db mysql -uroot -proot task5 < migrate.sql
+    docker exec -i task6-db mysql -uroot -proot task6 < migrate.sql
     curl http://localhost/alive
 EOF
